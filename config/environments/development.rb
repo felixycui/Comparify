7c8f7369b99cb41c332014d9670d1bfbe34ed35f
Felix <felixcantdebate@gmail.com>
2018-12-11 03:52:14 -0500

    Trying to use ENV

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,9 +1,9 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  
+
   config.read_encrypted_secrets = true
-  RSpotify::authenticate(Rails.application.secrets.spotify_client_id,
-                         Rails.application.secrets.spotify_client_secret)
+  RSpotify::authenticate(ENV['SPOTIFY_CLIENT_ID'],
+                         ENV['SPOTIFY_CLIENT_SECRET'])
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
